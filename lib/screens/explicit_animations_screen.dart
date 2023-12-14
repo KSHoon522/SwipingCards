e18c5a289c7dd6c8b1a1b0c2c03088578f156243
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-14 17:06:45 +0900

    Animated Builder

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ExplicitAnimationsScreen extends StatefulWidget {
@@ -8,12 +10,75 @@ class ExplicitAnimationsScreen extends StatefulWidget {
       _ExplicitAnimationsScreenState();
 }
 
-class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> {
+class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+  );
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Explicit Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: _animationController.value,
+                  child: Container(
+                    color: Colors.amber,
+                    width: 400,
+                    height: 400,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _play,
+                  child: const Text("Play"),
+                ),
+                ElevatedButton(
+                  onPressed: _pause,
+                  child: const Text("Pause"),
+                ),
+                ElevatedButton(
+                  onPressed: _rewind,
+                  child: const Text("Rewind"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
