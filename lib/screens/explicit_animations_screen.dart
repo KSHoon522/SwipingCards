80ca575ab3c660d874416490538fa79c14a68bce
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-15 10:12:42 +0900

    Explicit Widgets

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -15,9 +15,30 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     duration: const Duration(seconds: 2),
   );
 
-  late final Animation<Color?> _color = ColorTween(
-    begin: Colors.amber,
-    end: Colors.red,
+  late final Animation<Decoration> _decoration = DecorationTween(
+    begin: BoxDecoration(
+      color: Colors.amber,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    end: BoxDecoration(
+      color: Colors.red,
+      borderRadius: BorderRadius.circular(120),
+    ),
+  ).animate(_animationController);
+
+  late final Animation<double> _rotation = Tween(
+    begin: 0.0,
+    end: 2.0,
+  ).animate(_animationController);
+
+  late final Animation<double> _scale = Tween(
+    begin: 1.0,
+    end: 0.5,
+  ).animate(_animationController);
+
+  late final Animation<Offset> _offset = Tween(
+    begin: Offset.zero,
+    end: const Offset(0, -0.2),
   ).animate(_animationController);
 
   void _play() {
@@ -33,8 +54,9 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
   }
 
   @override
-  void initState() {
-    super.initState();
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -47,15 +69,21 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedBuilder(
-              animation: _color,
-              builder: (context, child) {
-                return Container(
-                  color: _color.value,
-                  width: 400,
-                  height: 400,
-                );
-              },
+            SlideTransition(
+              position: _offset,
+              child: ScaleTransition(
+                scale: _scale,
+                child: RotationTransition(
+                  turns: _rotation,
+                  child: DecoratedBoxTransition(
+                    decoration: _decoration,
+                    child: const SizedBox(
+                      height: 400,
+                      width: 400,
+                    ),
+                  ),
+                ),
+              ),
             ),
             const SizedBox(
               height: 30,
