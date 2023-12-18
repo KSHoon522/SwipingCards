88a3df728a8dfed1eacc26034e9d59e4862973d6
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-18 16:36:54 +0900

    Code Challenge

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -15,7 +15,7 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
   late final AnimationController _position = AnimationController(
     value: 0.0,
     vsync: this,
-    duration: const Duration(seconds: 1),
+    duration: const Duration(milliseconds: 800),
     lowerBound: (size.width + 100) * -1,
     upperBound: (size.width + 100),
   );
@@ -28,6 +28,19 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
   late final Tween<double> _scale = Tween(
     begin: 0.8,
     end: 1.0,
+  );
+
+  late final Tween<double> _buttonscale = Tween(
+    begin: 1.0,
+    end: 1.2,
+  );
+  late final ColorTween _closeColor = ColorTween(
+    begin: Colors.white,
+    end: Colors.red.shade300,
+  );
+  late final ColorTween _checkColor = ColorTween(
+    begin: Colors.white,
+    end: Colors.green.shade300,
   );
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
@@ -44,17 +57,24 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    final bound = size.width - 150;
+    final bound = size.width - 170;
     final dropZone = size.width + 100;
     if (_position.value.abs() >= bound) {
-      if (_position.value.isNegative) {
-        _position.animateTo(dropZone * -1).whenComplete(_whenComplete);
-      } else {
-        _position.animateTo(dropZone).whenComplete(_whenComplete);
-      }
+      final factor = _position.value.isNegative ? -1 : 1;
+      _position
+          .animateTo(dropZone * factor, curve: Curves.easeOut)
+          .whenComplete(_whenComplete);
     } else {
       _position.animateTo(0, curve: Curves.easeOut);
     }
+  }
+
+  void _onClick({required bool forward}) {
+    final dropZone = size.width + 100;
+    final factor = forward ? 1 : -1;
+    _position
+        .animateTo(dropZone * factor, curve: Curves.easeOut)
+        .whenComplete(_whenComplete);
   }
 
   @override
@@ -68,6 +88,7 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Swiping Cards'),
@@ -80,14 +101,20 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
               ) *
               pi /
               180;
-          final scale = _scale.transform(_position.value.abs() / size.width);
+          final scale =
+              _scale.transform(_position.value.abs() / (size.width + 100));
+          final buttonScale = min(
+              _buttonscale
+                  .transform(_position.value.abs() / (size.width + 100)),
+              1.2);
+          final bound = size.width - 170;
           return Stack(
             alignment: Alignment.topCenter,
             children: [
               Positioned(
                 top: 100,
                 child: Transform.scale(
-                  scale: scale,
+                  scale: min(scale, 1.0),
                   child: Card(
                     index: _index == 5 ? 1 : _index + 1,
                   ),
@@ -107,6 +134,90 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
                       ),
                     ),
                   ),
+                ),
+              ),
+              Positioned(
+                bottom: 100,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () => _onClick(forward: false),
+                      child: Transform.scale(
+                        scale: _position.value.isNegative ? buttonScale : 1.0,
+                        child: Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 5,
+                            ),
+                            borderRadius: BorderRadius.circular(100),
+                            color: _position.value.isNegative
+                                ? _closeColor.transform(
+                                    _position.value.abs() / (size.width + 100))
+                                : Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.7),
+                                blurRadius: 4.0,
+                                spreadRadius: 0.0,
+                                offset: const Offset(0, 5.0),
+                              ),
+                            ],
+                          ),
+                          child: Icon(
+                            Icons.close_rounded,
+                            color: _position.value.isNegative &&
+                                    _position.value.abs() > bound
+                                ? Colors.white
+                                : Colors.red,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () => _onClick(forward: true),
+                      child: Transform.scale(
+                        scale: !_position.value.isNegative ? buttonScale : 1.0,
+                        child: Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 5,
+                            ),
+                            borderRadius: BorderRadius.circular(100),
+                            color: !_position.value.isNegative
+                                ? _checkColor.transform(
+                                    _position.value.abs() / (size.width + 100))
+                                : Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.7),
+                                blurRadius: 4.0,
+                                spreadRadius: 0.0,
+                                offset: const Offset(0, 5.0),
+                              ),
+                            ],
+                          ),
+                          child: Icon(
+                            Icons.check_rounded,
+                            color: !_position.value.isNegative &&
+                                    _position.value.abs() > bound
+                                ? Colors.white
+                                : Colors.green,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
